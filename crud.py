2c2723918312c4f64ce5760ce5233bfbb9b286a7
Adam R Moss <adam.moss@hackbrightacademy.com>
2022-12-08 18:35:03 -0500

    Feature: Project Kickoff! (#1)
    * Add pip requirements
    
    * Add stub files for project
    
    * Sketch out some documentation

---
+++ crud.py
@@ -0,0 +1,14 @@
+"""CRUD operations."""
+
+from model import db, connect_to_db
+
+# TODO: add CRUD operations, as needed
+
+if __name__ == '__main__':
+    from server import app
+
+    # Call connect_to_db(app, echo=False) if your program output gets
+    # too annoying; this will tell SQLAlchemy not to print out every
+    # query it executes.
+
+    connect_to_db(app)
---
+++ database.py
@@ -0,0 +1,1 @@
+db_name='90-day-companion'
