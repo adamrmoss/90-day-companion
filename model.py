7c4b6b4ec032b67754cbfc7b72cd765da679bd4d
Adam R Moss <adamrmoss@gmail.com>
2022-12-08 19:31:50 -0500

    Create basics of User model

--- model.py
+++ model.py
@@ -6,23 +6,35 @@ from database import db_name
 db = SQLAlchemy()
 
 class User(db.Model):
-    pass
+    """90 Day Companion User"""
+    __tablename__ = 'users'
+
+    user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(128), nullable=False, unique=True)
+    password = db.Column(db.String(64), nullable=False)
+
+    def __repr__(self):
+        return f'<User user_id={self.user_id} email={self.email}>'
 
 
-class TvSeries(db.Model):
-    pass
+# class TvSeries(db.Model):
+#     """The 90 Day Fiancé series or one of its spinoffs"""
+#     pass
 
 
-class TvSeason(db.Model):
-    pass
+# class TvSeason(db.Model):
+#     """A season of a particular series"""
+#     pass
 
 
-class TvEpisode(db.Model):
-    pass
+# class TvEpisode(db.Model):
+#     """An episode of a particular series and season"""
+#     pass
 
 
-class TvStar(db.Model):
-    pass
+# class TvStar(db.Model):
+#     """A television star appearing on 90 Day Fiancé and / or other series"""
+#     pass
 
 
 def connect_to_db(flask_app, db_name=db_name, echo=True):
--- seed-database.py
+++ seed-database.py
@@ -14,5 +14,10 @@ model.connect_to_db(server.app)
 # Updates the database schema
 model.db.create_all()
 
+from crud import create_user
+
+# Create users
+admin = create_user('admin@90day.org', 'abc123')
+
 # TODO: use create methods to add objects to database, committing
 #       database sessions as needed
